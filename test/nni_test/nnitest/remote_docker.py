03a0c78a8ce8cc5880fa6ea46d8dfbbc10c5b641
liuzhe <zhe.liu@microsoft.com>
2020-11-16 14:42:08 +0800

    revert docker port

--- test/nni_test/nnitest/remote_docker.py
+++ test/nni_test/nnitest/remote_docker.py
@@ -34,7 +34,7 @@ def find_wheel_package(dir):
     return None
 
 def start_container(image, name, nnimanager_os):
-    '''Start docker container, print the port to stdout'''
+    '''Start docker container, generate a port in /tmp/nnitest/{name}/port file'''
     port = find_port()
     source_dir = '/tmp/nnitest/' + name
     run_cmds = ['docker', 'run', '-d', '-t', '-p', str(port) + ':22', '--name', name, '--mount', 'type=bind,source=' + source_dir + ',target=/tmp/nni', image]
@@ -61,7 +61,8 @@ def start_container(image, name, nnimanager_os):
     check_call(pip_cmds)
     sdk_cmds = ['docker', 'exec', name, 'python3', '-m', 'pip', 'install', get_dist(wheel_name)]
     check_call(sdk_cmds)
-    print(port)
+    with open(source_dir + '/port', 'w') as file:
+        file.write(str(port))
 
 def stop_container(name):
     '''Stop docker container'''
@@ -72,10 +73,10 @@ def stop_container(name):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('mode', choices=['start', 'stop'])
-    parser.add_argument('name')
-    parser.add_argument('image', nargs='?')
-    parser.add_argument('os', nargs='?', choices=['unix', 'windows'])
+    parser.add_argument('--mode', required=True, choices=['start', 'stop'], dest='mode', help='start or stop a container')
+    parser.add_argument('--name', required=True, dest='name', help='the name of container to be used')
+    parser.add_argument('--image', dest='image', help='the image to be used')
+    parser.add_argument('--os', dest='os', default='unix', choices=['unix', 'windows'], help='nniManager os version')
     args = parser.parse_args()
     if args.mode == 'start':
         start_container(args.image, args.name, args.os)
